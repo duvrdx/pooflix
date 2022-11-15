64143ce19820ae7ebefde5c171d62161667e6488
duvrdx <duvrdx@gmail.com>
2022-11-15 15:06:40 -0300

    fix

--- persistencia/ConexaoDAO.java
+++ persistencia/ConexaoDAO.java
@@ -2,17 +2,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 import javax.swing.JOptionPane;
 
 public class ConexaoDAO {
     public Connection conectaBD(){
         Connection conn = null;
-
-        Dotenv dotenv = Dotenv.load();
-        System.out.println(dotenv.get("TEST_KEY"));
-        
 
         try {
             String url = "jdbc:mysql://localhost:3306/database?user=&password=";
